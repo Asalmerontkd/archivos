cdac6cfabb978ad990488490115987694fee5e1f
Asalmerontkd <psrantonio@gmail.com>
2017-02-07 16:12:25 -0600

    warning

--- conexion.php
+++ conexion.php
@@ -5,7 +5,7 @@
 	$user='root';
 	$pass='toor';
 
-	$conn= mysql_pconnect($hostname, $user, $pass) or trigger_error(mysql_error(),E_USER_ERROR); 
+	$conn= @mysql_connect($hostname, $user, $pass) or trigger_error(mysql_error(),E_USER_ERROR); 
 	mysql_select_db($database);
 	error_reporting(E_ERROR | E_PARSE);
 ?>
